04baa8f62d2705299c57d3172bf1630ee0fbdbe3
laureabk6 <laure.abk6@gmail.com>
2020-05-18 15:19:29 +0100

    Completed module

--- stats/pitching.py
+++ stats/pitching.py
@@ -4,4 +4,11 @@ from data import games
 
 #condense the data to just rows of type play
 #games[] : shortcut to access the dataframe games
-#plays = games[type == 'play']
+#check if the 'type' column's value equals 'play'
+#select just the rows that have a 'type' of 'play'
+#type is a column
+plays = games[games['type'] == 'play']
+#select all rows that contain 'K' in 'event' column
+strike_outs = plays[plays['event'].str.contains('K')]
+#group the strike_outs dataframe by year and then by game_id
+strike_outs = strike_outs.groupby(['year','game_id']).size()
