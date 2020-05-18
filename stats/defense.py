6cb6766befe7a6b0f54db93b769b72a309334067
laureabk6 <laure.abk6@gmail.com>
2020-05-18 17:23:42 +0100

    Completed module

--- stats/defense.py
+++ stats/defense.py
@@ -33,3 +33,23 @@ events.columns = events.columns.droplevel()
 events.columns = ['year','game_id','team','BB','E','H','HBP','HR','ROE','SO']
 #remove label of the index using rename_axis(): pass in label of None, on the column axis
 events = events.rename_axis(None,axis='columns')
+
+#pa dataframe has columns year, game_id, team, PA
+#events dataframe has year, game_id, team, and a column for every event_type
+#merge them together with pd.merge()
+    #columns to merge: year, game_id, team
+events_plus_pa = pd.merge(events,pa, how='outer', left_on=['year','game_id','team'],
+                          right_on=['year','game_id','team'])
+defense = pd.merge(events_plus_pa,info) #info: home team vs visiting team
+#calculate the DER of each team
+defense.loc[:,'DER'] = 1 - ((defense['H']+defense['ROE'])/(defense['PA']-defense['BB']-defense['SO']-defense['HBP']-defense['HR']))
+#convert the year column to numeric values
+defense.loc[:,'year'] = pd.to_numeric(defense['year'])
+
+#plot the DER of the all-star teams in the last 40 years
+#select rows with year>1978 and also only keep certain columns
+der = defense.loc[defense['year'] >= 1978, ['year','defense','DER']]
+#call pivot to adjust the data for plotting
+der = der.pivot(index='year',columns='defense',values='DER')
+der.plot(x_compat=True,xticks=range(1978,2018,4),rot=45)
+plt.show()
