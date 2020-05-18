43ff2c3772a84bc6fc0c704facc2ad0cc64fe693
laureabk6 <laure.abk6@gmail.com>
2020-05-18 16:38:19 +0100

    Completed module

--- stats/defense.py
+++ stats/defense.py
@@ -5,4 +5,14 @@ from frames import games, info, events
 
 #query function to select all rows of 'games' dataframe
     #that have a 'type' of 'play' but do NOT have 'NP' as 'event'
-plays = games.query("type == 'play' & event != 'NP' ")
+plays = games.query(" type == 'play' & event != 'NP' ")
+#adjust/rename column labels
+plays.columns = ['type','inning','team','player','count','pitches',
+                 'event','game_id','year']
+
+#remove consecutive duplicate rows that represent the same at bat
+    #shift() moves the index a specified amount up or down
+#select all Rows that do Not match a consecutive row in the player column
+#keep only a few specified columns
+pa = plays.loc[plays['player'].shift() != plays['player'] ,
+               ['year','game_id','inning','team','player']]
