38490f920c246b927d1006ad7661f2f2cfa99bd8
laureabk6 <laure.abk6@gmail.com>
2020-05-18 16:32:37 +0100

    Completed module

--- stats/defense.py
+++ stats/defense.py
@@ -1,2 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+#what is the defensive efficiency ration by league, since 1978
+from frames import games, info, events
+
+#query function to select all rows of 'games' dataframe
+    #that have a 'type' of 'play' but do NOT have 'NP' as 'event'
+plays = games.query("type == 'play' & event != 'NP' ")
