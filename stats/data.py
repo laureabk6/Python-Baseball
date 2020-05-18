463144b53105b7aec255b485158e6d30bfaa499d
laureabk6 <laure.abk6@gmail.com>
2020-05-18 14:19:21 +0100

    Completed module

--- stats/data.py
+++ stats/data.py
@@ -12,8 +12,8 @@ for game_file in game_files:
     game_frame = pd.read_csv(game_file,names = ['type','multi2','multi3','multi4','multi5','multi6','event'])
     game_frames.append(game_frame)
 
-games = pd.concat(game_frames) #large dataframe called games
-games.loc["??",["multi5"]] = ""
+# games = pd.concat(game_frames) #large dataframe called games
+# games.loc["??",["multi5"]] = ""
 
 # identifiers = games["multi2"].str.extract(r'(.LS(\d{4})\d{5})') #is a dataframe
 # #has 2 columns. for rows that match the regex, the row has the correct extracted values
