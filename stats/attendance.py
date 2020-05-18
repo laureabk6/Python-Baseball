e6331b9725f671475f4589fb866d1fe60154eb19
laureabk6 <laure.abk6@gmail.com>
2020-05-18 14:52:50 +0100

    Completed module

--- stats/attendance.py
+++ stats/attendance.py
@@ -9,6 +9,6 @@ from data import games #from file data.py import games dataframe
 #type multi2 multi3 ... year
 #info attendance 4522 ... 1946
 #select rows with info and attendance and only the year and multi3 columns
-attendance = games.loc[(games['type' == 'info']) & (games['multi2']=='attendance'),['year','multi3']]
+attendance = games.loc[(games['type'] == 'info') & (games['multi2']=='attendance'),['year','multi3']]
 #change column labels
 attendance.columns = ['year','attendance']
