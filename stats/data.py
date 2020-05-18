18f1830bec75b8dae172fdbd87e749a66255e5b0
laureabk6 <laure.abk6@gmail.com>
2020-05-18 14:16:17 +0100

    Completed module

--- stats/data.py
+++ stats/data.py
@@ -8,24 +8,24 @@ game_files.sort()
 
 game_frames = list()
 
-for game_file in game_files:
-    game_frame = pd.read_csv(game_file,names = ['type','multi2','multi3','multi4','multi5','multi6','event'])
-    game_frames.append(game_frame)
-
-games = pd.concat(game_frames) #large dataframe called games
-games.loc["??",["multi5"]] = ""
-
-identifiers = games["multi2"].str.extract(r'(.LS(\d{4})\d{5})') #is a dataframe
-#has 2 columns. for rows that match the regex, the row has the correct extracted values
-identifiers = identifiers.fillna(method='ffill')#values must be filled in for all rows
-
-identifiers(columns=["game_id","year"])#change column labels
-games = concat([games,identifiers],axis=1,sort=False)#concatenate identifier columns:
-#append the columns of identifiers to games
-games = games.fillna(' ')#fill Nan values
-
-#reduce memory used by games, provide Pandas with clue to what data is contained in certain columns
-#select all rows, and just the 'type' column with loc function
-pd.Categorical(games.loc[:,['type']])
-
-print(games.head(5)) #print first 5 rows
+# for game_file in game_files:
+#     game_frame = pd.read_csv(game_file,names = ['type','multi2','multi3','multi4','multi5','multi6','event'])
+#     game_frames.append(game_frame)
+#
+# games = pd.concat(game_frames) #large dataframe called games
+# games.loc["??",["multi5"]] = ""
+#
+# identifiers = games["multi2"].str.extract(r'(.LS(\d{4})\d{5})') #is a dataframe
+# #has 2 columns. for rows that match the regex, the row has the correct extracted values
+# identifiers = identifiers.fillna(method='ffill')#values must be filled in for all rows
+#
+# identifiers(columns=["game_id","year"])#change column labels
+# games = concat([games,identifiers],axis=1,sort=False)#concatenate identifier columns:
+# #append the columns of identifiers to games
+# games = games.fillna(' ')#fill Nan values
+#
+# #reduce memory used by games, provide Pandas with clue to what data is contained in certain columns
+# #select all rows, and just the 'type' column with loc function
+# pd.Categorical(games.loc[:,['type']])
+#
+# print(games.head(5)) #print first 5 rows
