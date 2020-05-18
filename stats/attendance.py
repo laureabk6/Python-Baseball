b0c22f06d915fa8d4e2bfcfab9b0e82fdf7d6087
laureabk6 <laure.abk6@gmail.com>
2020-05-18 14:51:48 +0100

    Completed module

--- stats/attendance.py
+++ stats/attendance.py
@@ -4,3 +4,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from data import games #from file data.py import games dataframe
+
+#games contains the attendance for each game
+#type multi2 multi3 ... year
+#info attendance 4522 ... 1946
+#select rows with info and attendance and only the year and multi3 columns
+attendance = games.loc[(games['type' == 'info']) & (games['multi2']=='attendance'),['year','multi3']]
+#change column labels
+attendance.columns = ['year','attendance']
