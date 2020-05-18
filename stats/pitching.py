ce43e809142fa9bedab3ba2538a1c1df9520a59d
laureabk6 <laure.abk6@gmail.com>
2020-05-18 15:12:21 +0100

    Completed module

--- stats/pitching.py
+++ stats/pitching.py
@@ -1,2 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from data import games
+
+#condense the data to just rows of type play
+#games[] : shortcut to access the dataframe games
+#plays = games[type == 'play']
